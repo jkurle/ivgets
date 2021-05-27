ed11c215266ebc154644fed551a71fee9957ddc8
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2021-05-27 12:35:08 +0100

    benchmark to improve speed

---
+++ baseline/benchmarking.R
@@ -0,0 +1,11 @@
+# before any changes
+
+load("./data/artificial2sls_contaminated.rda")
+fml <- y ~ -1+x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11 | -1+x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+z11+z12
+base <- ivreg(formula = fml, data = artificial2sls_contaminated)
+# do impulse indicator saturation
+
+library(microbenchmark)
+
+before <- microbenchmark(isat(base, iis = TRUE, t.pval = 1/100, print.searchinfo = FALSE), times = 10)
+save(before, file = "./baseline/before")
