d6a5e8c60af317f278a2f33a3c56798a23f5ab99
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2022-05-25 11:52:41 +0100

    update description of artificial datasets

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 #' regressors, and excluded instruments for 2SLS models. The structural error is
 #' also stored even though not observed in practice.
 #'
-#' @format A data frame with 100 observations (rows) and 15 variables (columns):
+#' @format A data frame with 100 observations (rows) and 16 variables (columns):
 #' \describe{
 #'   \item{y}{dependent variable}
 #'   \item{x1}{intercept}
@@ -13,6 +13,7 @@
 #'   \item{x11}{relevant endogenous regressor}
 #'   \item{u}{structural error (in practice unobserved)}
 #'   \item{z11 - z12}{excluded instruments}
+#'   \item{id}{unique observation identifier}
 #' }
 "artificial2sls"
 
@@ -24,7 +25,7 @@
 #' also stored even though not observed in practice. Some errors are
 #' contaminated, making these observations outliers.
 #'
-#' @format A data frame with 100 observations (rows) and 15 variables (columns):
+#' @format A data frame with 100 observations (rows) and 16 variables (columns):
 #' \describe{
 #'   \item{y}{dependent variable}
 #'   \item{x1}{intercept}
@@ -33,6 +34,7 @@
 #'   \item{x11}{relevant endogenous regressor}
 #'   \item{u}{structural error (in practice unobserved)}
 #'   \item{z11 - z12}{excluded instruments}
+#'   \item{id}{unique observation identifier}
 #' }
 #'
 #' @details The data frame has two additional attributes that store the indices
@@ -41,10 +43,19 @@
 #'
 "artificial2sls_contaminated"
 
-#' Artificial data set prepared for shiny application
+#' Artificial data set without outliers prepared for shiny application.
 #'
-#' Similar to "artificial2sls" with a few minor additions.
-#'
-#' @format A data frame with 100 observations (rows) and 17 variables (columns)
+#' @format A data frame with 100 observations (rows) and 17 variables (columns):
+#' \describe{
+#'   \item{y}{dependent variable}
+#'   \item{x1}{intercept}
+#'   \item{x2}{relevant exogenous regressor}
+#'   \item{x3 - x10}{irrelevant exogenous regressors}
+#'   \item{x11}{relevant endogenous regressor}
+#'   \item{u}{structural error (in practice unobserved)}
+#'   \item{z11 - z12}{excluded instruments}
+#'   \item{id}{unique observation identifier}
+#'   \item{is.outlier}{factor variable whether the observation is an outlier (\code{1}) or not (\code{0})}
+#' }
 "artificial2sls_shiny"
 
