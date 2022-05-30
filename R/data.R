0f920d9e4467920cc495c51973ffb3c7a86fad46
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2022-05-30 17:51:06 +0100

    update dataset documentation to comply with CRAN

--- R/data.R
+++ R/data.R
@@ -5,15 +5,24 @@
 #' also stored even though not observed in practice.
 #'
 #' @format A data frame with 100 observations (rows) and 16 variables (columns):
-#' \describe{
-#'   \item{y}{dependent variable}
-#'   \item{x1}{intercept}
-#'   \item{x2}{relevant exogenous regressor}
-#'   \item{x3 - x10}{irrelevant exogenous regressors}
-#'   \item{x11}{relevant endogenous regressor}
-#'   \item{u}{structural error (in practice unobserved)}
-#'   \item{z11 - z12}{excluded instruments}
-#'   \item{id}{unique observation identifier}
+#' \tabular{rl}{
+#'   \strong{name} \tab \strong{variable description} \cr
+#'   y \tab dependent variable \cr
+#'   x1 \tab intercept \cr
+#'   x2 \tab relevant exogenous regressor \cr
+#'   x3 \tab irrelevant exogenous regressor \cr
+#'   x4 \tab irrelevant exogenous regressor \cr
+#'   x5 \tab irrelevant exogenous regressor \cr
+#'   x6 \tab irrelevant exogenous regressor \cr
+#'   x7 \tab irrelevant exogenous regressor \cr
+#'   x8 \tab irrelevant exogenous regressor \cr
+#'   x9 \tab irrelevant exogenous regressor \cr
+#'   x10 \tab irrelevant exogenous regressor \cr
+#'   x11 \tab relevant endogenous regressor \cr
+#'   u \tab structural error (in practice unobserved) \cr
+#'   z11 \tab excluded instrument \cr
+#'   z12 \tab excluded instrument \cr
+#'   id \tab unique observation identifier
 #' }
 "artificial2sls"
 
@@ -26,15 +35,24 @@
 #' contaminated, making these observations outliers.
 #'
 #' @format A data frame with 100 observations (rows) and 16 variables (columns):
-#' \describe{
-#'   \item{y}{dependent variable}
-#'   \item{x1}{intercept}
-#'   \item{x2}{relevant exogenous regressor}
-#'   \item{x3 - x10}{irrelevant exogenous regressors}
-#'   \item{x11}{relevant endogenous regressor}
-#'   \item{u}{structural error (in practice unobserved)}
-#'   \item{z11 - z12}{excluded instruments}
-#'   \item{id}{unique observation identifier}
+#' \tabular{rl}{
+#'   \strong{name} \tab \strong{variable description} \cr
+#'   y \tab dependent variable \cr
+#'   x1 \tab intercept \cr
+#'   x2 \tab relevant exogenous regressor \cr
+#'   x3 \tab irrelevant exogenous regressor \cr
+#'   x4 \tab irrelevant exogenous regressor \cr
+#'   x5 \tab irrelevant exogenous regressor \cr
+#'   x6 \tab irrelevant exogenous regressor \cr
+#'   x7 \tab irrelevant exogenous regressor \cr
+#'   x8 \tab irrelevant exogenous regressor \cr
+#'   x9 \tab irrelevant exogenous regressor \cr
+#'   x10 \tab irrelevant exogenous regressor \cr
+#'   x11 \tab relevant endogenous regressor \cr
+#'   u \tab structural error (in practice unobserved) \cr
+#'   z11 \tab excluded instrument \cr
+#'   z12 \tab excluded instrument \cr
+#'   id \tab unique observation identifier
 #' }
 #'
 #' @details The data frame has two additional attributes that store the indices
@@ -46,16 +64,25 @@
 #' Artificial data set without outliers prepared for shiny application.
 #'
 #' @format A data frame with 100 observations (rows) and 17 variables (columns):
-#' \describe{
-#'   \item{y}{dependent variable}
-#'   \item{x1}{intercept}
-#'   \item{x2}{relevant exogenous regressor}
-#'   \item{x3 - x10}{irrelevant exogenous regressors}
-#'   \item{x11}{relevant endogenous regressor}
-#'   \item{u}{structural error (in practice unobserved)}
-#'   \item{z11 - z12}{excluded instruments}
-#'   \item{id}{unique observation identifier}
-#'   \item{is.outlier}{factor variable whether the observation is an outlier (\code{1}) or not (\code{0})}
+#' \tabular{rl}{
+#'   \strong{name} \tab \strong{variable description} \cr
+#'   y \tab dependent variable \cr
+#'   x1 \tab intercept \cr
+#'   x2 \tab relevant exogenous regressor \cr
+#'   x3 \tab irrelevant exogenous regressor \cr
+#'   x4 \tab irrelevant exogenous regressor \cr
+#'   x5 \tab irrelevant exogenous regressor \cr
+#'   x6 \tab irrelevant exogenous regressor \cr
+#'   x7 \tab irrelevant exogenous regressor \cr
+#'   x8 \tab irrelevant exogenous regressor \cr
+#'   x9 \tab irrelevant exogenous regressor \cr
+#'   x10 \tab irrelevant exogenous regressor \cr
+#'   x11 \tab relevant endogenous regressor \cr
+#'   u \tab structural error (in practice unobserved) \cr
+#'   z11 \tab excluded instrument \cr
+#'   z12 \tab excluded instrument \cr
+#'   id \tab unique observation identifier \cr
+#'   is.outlier \tab factor variable whether the observation is an outlier (\code{1}) or not (\code{0})
 #' }
 "artificial2sls_shiny"
 
